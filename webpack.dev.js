34c3294b0d566dcca73e4b7bb01aec1f0a5fd1b7
tangsj <t_fate@163.com>
2017-11-24 21:49:55 +0800

    获取 电脑ip

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@
  * @author tangsj
  */
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
@@ -11,12 +12,22 @@ const baseConfig = require('./webpack.config');
 
 const env = process.env.NODE_ENV;
 
+let hostip = '0.0.0.0';
+const ifaces = os.networkInterfaces();
+for (var dev in ifaces) {
+  ifaces[dev].forEach(function (details, alias) {
+    if (details.family == 'IPv4' && details.addres != '127.0.0.1') {
+      hostip = details.address;
+    }
+  });
+}
+
 const config = {
   // 控制是否生成，以及如何生成 source map。 http://www.css88.com/doc/webpack/configuration/devtool/
   devtool: 'eval',
   // 开发模式
   devServer: {
-    host: '0.0.0.0',
+    host: hostip,
     // contentBase: './dist',
     hot: true,
     inline: true,
